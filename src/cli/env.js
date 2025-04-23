edc30eee834c7c3f86ce17a4fa7f941ad4e6ed6d
LonelyBush <lonely.bush68@gmail.com>
2025-04-23 17:31:43 +0300

    feat: complete cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    let search = [];
+    for (let key in process.env) {
+        if(key.startsWith('RSS_')){
+            search.push(`${key}=${process.env[key]}`);
+        }
+      }
+      console.log(search.join('; '));
 };
 
 parseEnv();
