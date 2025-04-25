1ec7dc188927c71fc9dbab7321ca9e99b3f21f27
LonelyBush <lonely.bush68@gmail.com>
2025-04-25 13:36:55 +0300

    complete hash modules streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import {createReadStream} from 'fs'
+import crypto from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+   const fs = createReadStream('src/hash/files/fileToCalculateHashFor.txt',
+    {
+        encoding: 'utf-8',
+    },
+   )
+fs.on('data', (chunk) => {
+    var hash = crypto.createHash('sha256').update(chunk).digest('hex')
+    console.log(hash)
+})
 };
 
 await calculateHash();
--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,24 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.cjs';
+import object2 from "./files/a.json" with { type: "json" };
+import object3 from "./files/b.json" with { type: "json" };
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = object2
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = object3
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${path.__filename}`);
+console.log(`Path to current directory is ${path.__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
@@ -33,7 +35,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
